0c5287d42842565d352f1de5160271828dffa9aa
vikram kumar <vikram.spidy@gmail.com>
2019-02-24 10:51:20 +0530

    added category hero image.

--- src/containers/Category/category.js
+++ src/containers/Category/category.js
@@ -1,8 +1,23 @@
 import React, { Component } from "react";
+import "../../scss/categories.scss";
 
 export default class Category extends Component {
   componentDidMount() {}
   render() {
-    return <div>Category:{this.props.match.params.category}</div>;
+    const categoryName = this.props.match.params.category;
+
+    let backgroundImageURL = require(`../../images/category_${categoryName}.jpg`);
+    let heroStyle = {
+      backgroundImage: `url(${backgroundImageURL})`
+    };
+    return (
+      <div>
+        <section className="hero-section categories" style={heroStyle}>
+          <div className="container">
+            Category:{this.props.match.params.category}
+          </div>
+        </section>
+      </div>
+    );
   }
 }
