99e2940e817b9c2ea4c97ae633484683057a2574
Srinu <srinubcik@gmail.com>
2019-02-25 17:59:07 +0530

    add to cart added in product.js

--- src/components/Product/product.js
+++ src/components/Product/product.js
@@ -1,20 +1,36 @@
 import React, { Component } from "react";
+import * as Actions from "../../actions";
 
-export default class Product extends Component {
+import { connect } from "react-redux";
+
+class Product extends Component {
   constructor(props) {
     super(props);
-
-    this.state = { active: false };
   }
-  onPress() {
-    this.setState({ active: true });
-    if (this.state.active) {
-      return <p class="red">added to cart</p>;
+  componentDidMount() {}
+  addtoCart(e) {
+    let cart = localStorage.getItem("react-shop-cart");
+    let props = this.props;
+    console.log(this.props.product);
+    if (cart) {
+      cart = JSON.parse(cart);
+      props.AddToCart({
+        inCartId: cart.inCartId,
+        inProductId: this.props.product.product_id,
+        inAttributes: null
+      });
+    } else {
+      props.AddToCart({
+        inCartId: null,
+        inProductId: this.props.product.product_id,
+        inAttributes: null
+      });
     }
+    console.log("got in");
   }
   render() {
     return (
-      <div class="col-md-3 item">
+      <div class="col-md-4 item">
         <div class="hot_block">
           <img
             src={require(`../../images/product_images/${
@@ -22,12 +38,28 @@ export default class Product extends Component {
             }`)}
           />
           <h3 class="black">{this.props.product.name}</h3>
-          <p class="red">{"$" + this.props.product.price}</p>
-          <button type="button" class="btn btn-sm" onClick={this.onPress()}>
-            Add to cart
-          </button>
+          <p class="red">{"$" + this.props.product.name}</p>
+          <button onClick={this.addtoCart.bind(this)}>Add to cart</button>
         </div>
       </div>
     );
   }
 }
+
+const mapStateToProps = state => {
+  return {
+    cart: state.get("cart")
+  };
+};
+
+function mapDispatchToProps(dispatch) {
+  return {
+    AddToCart: (inCartId, inProductId, inAttributes) =>
+      dispatch(Actions.AddToCart.request(inCartId, inProductId, inAttributes))
+  };
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Product);
