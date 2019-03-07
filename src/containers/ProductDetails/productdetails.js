101a8ea10558dfd813747b617e05943119623329
Srinu <srinubcik@gmail.com>
2019-03-07 13:41:54 +0530

    product details updates

--- src/containers/ProductDetails/productdetails.js
+++ src/containers/ProductDetails/productdetails.js
@@ -7,7 +7,7 @@ import * as Actions from "../../actions";
 import "react-confirm-alert/src/react-confirm-alert.css";
 import AliceCarousel from "react-alice-carousel";
 import { BeatLoader } from "react-spinners";
-
+import { ClipLoader } from "react-spinners";
 const handleOnDragStart = e => e.preventDefault();
 
 class ProductDetails extends Component {
@@ -137,6 +137,9 @@ class ProductDetails extends Component {
     let productImg2 = this.props.productdetails[0]
       ? this.props.productdetails[0].image_2
       : "";
+    let productId = this.props.productlocations[0]
+      ? this.props.productlocations[0].product_id
+      : "";
     let productlocations = this.props.productlocations[0]
       ? this.props.productlocations[0].department_name.toLowerCase()
       : "";
@@ -146,223 +149,236 @@ class ProductDetails extends Component {
     let hasItems = cart.count > 0 ? true : false;
     let this_ref = this;
     console.log(this.props);
-    return (
-      <React.Fragment>
-        <div id="main" className="mt-5 mb-5">
-          <div className="container">
-            <div className="product-block bg-white">
-              <div className="row">
-                <div className="col-md-12 mb-3 breadcrumbs">
-                  <ul className="list-unstyled">
-                    <li>
-                      <LinkContainer to="/">
-                        <a>Home</a>
-                      </LinkContainer>
-                    </li>
-                    <li className="productlocations_breadcrumb">
-                      <div className="arrow">{" > "}</div>
-                      <LinkContainer to={"/categories/" + productlocations}>
-                        <a>
-                          {this.props.productlocations[0]
-                            ? this.props.productlocations[0].department_name
-                            : ""}
-                        </a>
-                      </LinkContainer>
-                    </li>
-                    <li className="productlocations_breadcrumb">
-                      <div className="arrow">{" > "}</div>
-
-                      {this.props.productlocations[0]
-                        ? this.props.productlocations[0].category_name
-                        : ""}
-                    </li>
-                  </ul>
-                  <div className="clearfix" />
-                </div>
-                <div className="col-md-6 item-left-block">
-                  <div className="top-image text-center">
-                    <img
-                      src={require(`../../images/product_images/${
-                        this.state.productImageName
-                          ? this.state.productImageName
-                          : this.props.productdetails[0]
-                            ? this.props.productdetails[0].image
-                            : "a-partridge-in-a-pear-tree-2.gif"
-                      }`)}
-                      alt="Image"
-                      title="Image"
-                    />
-                  </div>
-                  <div className="colors-selection-block">
+    if (this.state.link === this.props.match.params.productid) {
+      return (
+        <React.Fragment>
+          <div id="main" className="mt-5 mb-5">
+            <div className="container">
+              <div className="product-block bg-white">
+                <div className="row">
+                  <div className="col-md-12 mb-3 breadcrumbs">
                     <ul className="list-unstyled">
-                      <li
-                        className={
-                          this.state.productImageName === productImg1
-                            ? "active"
-                            : this.state.activeClass
-                        }
-                      >
-                        <a
-                          onClick={() => {
-                            this.handleClick(
-                              this.props.productdetails[0]
-                                ? this.props.productdetails[0].image
-                                : "a-partridge-in-a-pear-tree-2.gif"
-                            );
-                          }}
-                        >
-                          <img
-                            src={require(`../../images/product_images/${
-                              this.props.productdetails[0]
-                                ? this.props.productdetails[0].image
-                                : "a-partridge-in-a-pear-tree-2.gif"
-                            }`)}
-                            alt="Image"
-                            title="Image1"
-                          />
-                        </a>
+                      <li>
+                        <LinkContainer to="/">
+                          <a>Home</a>
+                        </LinkContainer>
                       </li>
+                      <li className="productlocations_breadcrumb">
+                        <div className="arrow">{" > "}</div>
+                        <LinkContainer to={"/categories/" + productlocations}>
+                          <a>
+                            {this.props.productlocations[0]
+                              ? this.props.productlocations[0].department_name
+                              : ""}
+                          </a>
+                        </LinkContainer>
+                      </li>
+                      <li className="productlocations_breadcrumb">
+                        <div className="arrow">{" > "}</div>
 
-                      <li
-                        className={
-                          this.state.productImageName === productImg2
-                            ? "active"
-                            : ""
-                        }
-                      >
-                        <a
-                          onClick={() => {
-                            this.handleClick(
-                              this.props.productdetails[0]
-                                ? this.props.productdetails[0].image_2
-                                : "a-partridge-in-a-pear-tree-2.gif"
-                            );
-                          }}
-                        >
-                          <img
-                            src={require(`../../images/product_images/${
-                              this.props.productdetails[0]
-                                ? this.props.productdetails[0].image_2
-                                : "a-partridge-in-a-pear-tree-2.gif"
-                            }`)}
-                            alt="Image"
-                            title="Image3"
-                          />
-                        </a>
+                        {this.props.productlocations[0]
+                          ? this.props.productlocations[0].category_name
+                          : ""}
                       </li>
                     </ul>
+                    <div className="clearfix" />
                   </div>
-                </div>
-                <div className="col-md-6 item-right-block ">
-                  <div className="item-title">
-                    <h2>
-                      {this.props.productdetails[0]
-                        ? this.props.productdetails[0].name
-                        : ""}
-                    </h2>
-                    <p>
-                      {this.props.productdetails[0]
-                        ? this.props.productdetails[0].description
-                        : ""}
-                    </p>
-                  </div>
-                  <div className="amount-block pt-3">
-                    <h6 className="pricetag"> Price:</h6>
-                    {this.props.productdetails[0]
-                      ? "$" + this.props.productdetails[0].price
-                      : ""}
-                    {this.props.productdetails[0] ? <p> {""}</p> : ""}
-                  </div>
-                  {this.props.productdetails[0] ? (
-                    <div className="amount-block pt-3 pb-3">
-                      <h6 className="pricetag"> Discounted Price:</h6>
-                      {this.props.productdetails[0]
-                        ? "$" + this.props.productdetails[0].discounted_price
-                        : ""}
+                  <div className="col-md-6 item-left-block">
+                    <div className="top-image text-center">
+                      <img
+                        src={require(`../../images/product_images/${
+                          this.state.productImageName
+                            ? this.state.productImageName
+                            : this.props.productdetails[0]
+                              ? this.props.productdetails[0].image
+                              : "a-partridge-in-a-pear-tree-2.gif"
+                        }`)}
+                        alt="Image"
+                        title="Image"
+                      />
                     </div>
-                  ) : (
-                    ""
-                  )}
+                    <div className="colors-selection-block">
+                      <ul className="list-unstyled">
+                        <li
+                          className={
+                            this.state.productImageName === productImg1
+                              ? "active"
+                              : this.state.activeClass
+                          }
+                        >
+                          <a
+                            onClick={() => {
+                              this.handleClick(
+                                this.props.productdetails[0]
+                                  ? this.props.productdetails[0].image
+                                  : "a-partridge-in-a-pear-tree-2.gif"
+                              );
+                            }}
+                          >
+                            <img
+                              src={require(`../../images/product_images/${
+                                this.props.productdetails[0]
+                                  ? this.props.productdetails[0].image
+                                  : "a-partridge-in-a-pear-tree-2.gif"
+                              }`)}
+                              alt="Image"
+                              title="Image1"
+                            />
+                          </a>
+                        </li>
 
-                  <div className="product_images_block">
-                    <AliceCarousel mouseDragEnabled>
-                      <img
-                        src={require(`../../images/product_images/${
-                          this.props.productdetails[0]
-                            ? this.props.productdetails[0].image
-                            : "a-partridge-in-a-pear-tree-2.gif"
-                        }`)}
-                        onDragStart={handleOnDragStart}
-                        className="yours-custom-class"
-                      />
-                      <img
-                        src={require(`../../images/product_images/${
-                          this.props.productdetails[0]
-                            ? this.props.productdetails[0].image_2
-                            : "a-partridge-in-a-pear-tree-2.gif"
-                        }`)}
-                        onDragStart={handleOnDragStart}
-                        className="yours-custom-class"
-                      />
-                    </AliceCarousel>
+                        <li
+                          className={
+                            this.state.productImageName === productImg2
+                              ? "active"
+                              : ""
+                          }
+                        >
+                          <a
+                            onClick={() => {
+                              this.handleClick(
+                                this.props.productdetails[0]
+                                  ? this.props.productdetails[0].image_2
+                                  : "a-partridge-in-a-pear-tree-2.gif"
+                              );
+                            }}
+                          >
+                            <img
+                              src={require(`../../images/product_images/${
+                                this.props.productdetails[0]
+                                  ? this.props.productdetails[0].image_2
+                                  : "a-partridge-in-a-pear-tree-2.gif"
+                              }`)}
+                              alt="Image"
+                              title="Image3"
+                            />
+                          </a>
+                        </li>
+                      </ul>
+                    </div>
                   </div>
-                  <div className="pt-5 btn-block">
-                    <button
-                      type="button"
-                      className="btn btn-lg"
-                      onClick={this.addtoCart.bind(this)}
-                    >
-                      Add to cart
-                    </button>
-                    <span className={"add_to_cart mt-2"}>
-                      <h3
-                        className={
-                          "addcart" + this.state.show ? this.state.show : ""
-                        }
+                  <div className="col-md-6 item-right-block ">
+                    <div className="item-title">
+                      <h2>
+                        {this.props.productdetails[0]
+                          ? this.props.productdetails[0].name
+                          : ""}
+                      </h2>
+                      <p>
+                        {this.props.productdetails[0]
+                          ? this.props.productdetails[0].description
+                          : ""}
+                      </p>
+                    </div>
+                    <div className="amount-block pt-3">
+                      <h6 className="pricetag"> Price:</h6>
+                      {this.props.productdetails[0]
+                        ? "$" + this.props.productdetails[0].price
+                        : ""}
+                      {this.props.productdetails[0] ? <p> {""}</p> : ""}
+                    </div>
+                    {this.props.productdetails[0] ? (
+                      <div className="amount-block pt-3 pb-3">
+                        <h6 className="pricetag"> Discounted Price:</h6>
+                        {this.props.productdetails[0]
+                          ? "$" + this.props.productdetails[0].discounted_price
+                          : ""}
+                      </div>
+                    ) : (
+                      ""
+                    )}
+
+                    <div className="product_images_block">
+                      <AliceCarousel mouseDragEnabled>
+                        <img
+                          src={require(`../../images/product_images/${
+                            this.props.productdetails[0]
+                              ? this.props.productdetails[0].image
+                              : "a-partridge-in-a-pear-tree-2.gif"
+                          }`)}
+                          onDragStart={handleOnDragStart}
+                          className="yours-custom-class"
+                        />
+                        <img
+                          src={require(`../../images/product_images/${
+                            this.props.productdetails[0]
+                              ? this.props.productdetails[0].image_2
+                              : "a-partridge-in-a-pear-tree-2.gif"
+                          }`)}
+                          onDragStart={handleOnDragStart}
+                          className="yours-custom-class"
+                        />
+                      </AliceCarousel>
+                    </div>
+                    <div className="pt-5 btn-block">
+                      <button
+                        type="button"
+                        className="btn btn-lg"
+                        onClick={this.addtoCart.bind(this)}
                       >
-                        {"Adding to cart"}
-                      </h3>
-                    </span>
+                        Add to cart
+                      </button>
+                      <span className={"add_to_cart mt-2"}>
+                        <h3
+                          className={
+                            "addcart" + this.state.show ? this.state.show : ""
+                          }
+                        >
+                          {"Adding to cart"}
+                        </h3>
+                      </span>
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
+
+            {this.props.productrecommendations.length > 0 ? (
+              <div className="container related-block mt-5">
+                <h2 className="pb-4">You may also like</h2>
+                <div className="row">
+                  {this.props.productrecommendations.map((item, index) => {
+                    return (
+                      <div className="col-sm-6 col-lg-3" key={index}>
+                        <LinkContainer
+                          to={
+                            "/product/" +
+                            item.product_id +
+                            "/" +
+                            item.product_name
+                          }
+                        >
+                          <a>
+                            <div className="product-image-block bg-white">
+                              <h3 className="pt-3">{item.product_name}</h3>
+                              <p className="pt-3">{item.description}</p>
+                              <div className="price pt-3">&#163;14.99</div>
+                            </div>
+                          </a>
+                        </LinkContainer>
+                      </div>
+                    );
+                  })}
+                </div>
+              </div>
+            ) : (
+              ""
+            )}
           </div>
-
-          {this.props.productrecommendations.length > 0 ? (
-            <div className="container related-block mt-5">
-              <h2 className="pb-4">You may also like</h2>
-              <div className="row">
-                {this.props.productrecommendations.map((item, index) => {
-                  return (
-                    <div className="col-sm-6 col-lg-3" key={index}>
-                      <LinkContainer
-                        to={
-                          "/product/" +
-                          item.product_id +
-                          "/" +
-                          item.product_name
-                        }
-                      >
-                        <a>
-                          <div className="product-image-block bg-white">
-                            <h3 className="pt-3">{item.product_name}</h3>
-                            <p className="pt-3">{item.description}</p>
-                            <div className="price pt-3">&#163;14.99</div>
-                          </div>
-                        </a>
-                      </LinkContainer>
-                    </div>
-                  );
-                })}
-              </div>
-            </div>
-          ) : (
-            ""
-          )}
+        </React.Fragment>
+      );
+    } else {
+      return (
+        <div className="clip_loader">
+          <ClipLoader
+            sizeUnit={"px"}
+            size={80}
+            color={"#f62f5e"}
+            loading={this.state.loading}
+          />
         </div>
-      </React.Fragment>
-    );
+      );
+    }
   }
 }
 const mapStateToProps = state => {
