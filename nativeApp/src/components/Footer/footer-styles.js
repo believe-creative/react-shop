d175fb66a49c173ce5d48cb360a4feffc9eee18e
Jagadish <jag@teamdevx.com>
2019-05-03 16:40:48 +0530

    Footer css updates.

--- nativeApp/src/components/Footer/footer-styles.js
+++ nativeApp/src/components/Footer/footer-styles.js
@@ -2,14 +2,31 @@ import {StyleSheet} from 'react-native';
 
 export const styles = StyleSheet.create({
   footer: {
-    flex: 0,
-    justifyContent: 'center',
-    alignItems: 'center',
     backgroundColor: '#eeeeee',
 	 fontSize: 16,
 	 padding: 10,
   },
 	footertext: {
 		color: '#333',
-	}
+		fontSize: 16,
+	},
+	item: {
+    padding: 10,
+    fontSize: 18,
+    height: 44,
+  },
+	block1: {
+		marginBottom: 15,
+	},
+	ftitle: {
+		fontSize: 22,
+		fontWeight: 'bold',
+		color: '#333',
+		marginBottom: 5,
+	},
+	footerlinks: {
+		fontSize: 16,
+		color: '#2e2e2e',
+		fontWeight: '400',
+	},
 });
--- nativeApp/src/components/Footer/footer.js
+++ nativeApp/src/components/Footer/footer.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {Platform, StyleSheet, Text, View, FlatList, TouchableOpacity, Linking } from 'react-native';
 import {styles} from './footer-styles'; 
 
 export default class Footer extends Component {
@@ -15,7 +15,35 @@ export default class Footer extends Component {
 
     return (
       <View style={styles.footer}>
-        <Text style={styles.footertext}>@ Belivie Creative</Text>
+		  <Text style={styles.ftitle}>Questions?</Text>		 
+		 <FlatList style={styles.block1}
+          data={[
+            {key: 'Help', url: 'http://google.com'},
+            {key: 'Track Order', url: 'http://yahoo.com'},
+            {key: 'Returns', url: 'http://facebook.com'},
+          ]}
+          renderItem={({item}) => <TouchableOpacity onPress={() => Linking.openURL(item.url)}><Text style={styles.footerlinks}>{item.key}</Text></TouchableOpacity> }
+        />
+		 <Text style={styles.ftitle}>WHAT'S IN STORE</Text>
+		 <FlatList style={styles.block1}
+          data={[
+            {key: 'Women', url: 'http://google.com'},
+            {key: 'Men', url: 'http://yahoo.com'},
+            {key: 'Product A-Z', url: 'http://facebook.com'},
+				{key: 'Buy Gift Vouchers', url: 'http://google.com'},
+          ]}
+          renderItem={({item}) => <TouchableOpacity onPress={() => Linking.openURL(item.url)}><Text style={styles.footerlinks}>{item.key}</Text></TouchableOpacity> }
+        />
+		  <Text style={styles.ftitle}>Follow Us</Text>
+		 <FlatList style={styles.block1}
+          data={[
+            {key: 'Facebook', url: 'http://google.com'},
+            {key: 'Twitter', url: 'http://yahoo.com'},
+            {key: 'Youtube', url: 'http://facebook.com'},
+          ]}
+          renderItem={({item}) => <TouchableOpacity onPress={() => Linking.openURL(item.url)}><Text style={styles.footerlinks}>{item.key}</Text></TouchableOpacity> }
+        />
+		 <TouchableOpacity onPress={() => Linking.openURL('https://www.believecreative.com/')}><Text style={styles.footertext}>Developed by Believe Creative</Text></TouchableOpacity>
       </View>
     );
   }
