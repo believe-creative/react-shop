20cf147f93e3a2a2a8747b3d591d18b84caef794
Srinu <srinubcik@gmail.com>
2019-05-10 10:55:09 +0530

    nav bar menu toggle is updated

--- nativeApp/src/routes/navigationRoutes.js
+++ nativeApp/src/routes/navigationRoutes.js
@@ -18,37 +18,6 @@ export const RootStack = createStackNavigator(
   },
   {
     initialRouteName: 'Home',
-    
+
   }
 );
-
-// const Nav = DrawerNavigator(this.getNavItems(this.state.drawerSource.navItems));
-//
-//         const AppNavigator = (StackNavigator(
-//             {
-//                 Drawer: { screen: Nav },
-//             },
-//             {
-//                 initialRouteName: "Drawer",
-//                 headerMode: "none",
-//             })
-//         );
-//
-//         return (<AppNavigator />);
-//
-// getNavItems(navItems) {
-//     return navItems.reduce((routes, navItem) => {
-//         routes[navItem.id] = this.getNavItem(navItem);
-//
-//         return routes;
-//     }, {});
-// }
-//
-// getNavItem(navItem) {
-//     return {
-//         screen: (props) => <MyScreen {...props} id={navItem.id} />,
-//         navigationOptions: {
-//             ***any navigation properties
-//         }
-//     }
-// }
