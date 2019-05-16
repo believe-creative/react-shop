683954e49e483dd7d9b1ce4613251115427e201e
Jagadish <jag@teamdevx.com>
2019-05-16 09:59:07 +0530

    Footer updates.

--- nativeApp/src/components/Footer/footer.js
+++ nativeApp/src/components/Footer/footer.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View, FlatList, TouchableOpacity, Linking,ScrollView } from 'react-native';
 import {styles} from './footer-styles';
+import NavigationService from '../../routes/NavigationService.js'; 
 
 export default class Footer extends Component {
 
@@ -12,29 +13,14 @@ export default class Footer extends Component {
   }
 
   render() {
-
+ 
     return (
       <ScrollView style={styles.footer}>
-		  <Text style={styles.ftitle}>Questions?</Text>
-		 <FlatList style={styles.block1}
-          data={[
-            {key: 'Help', url: '#'},
-            {key: 'Track Order', url: '#'},
-            {key: 'Returns', url: '#'},
-          ]}
-          renderItem={({item}) => <TouchableOpacity onPress={() => Linking.openURL(item.url)}><Text style={styles.footerlinks}>{item.key}</Text></TouchableOpacity> }
-        />
 		 <Text style={styles.ftitle}>WHAT'S IN STORE</Text>
-		 <FlatList style={styles.block1}
-          data={[
-            {key: 'Women', url: '#'},
-            {key: 'Men', url: '#'},
-            {key: 'Product A-Z', url: '#'},
-				{key: 'Buy Gift Vouchers', url: '#'},
-          ]}
-          renderItem={({item}) => <TouchableOpacity onPress={() => Linking.openURL(item.url)}><Text style={styles.footerlinks}>{item.key}</Text></TouchableOpacity> }
-        />
-		  <Text style={styles.ftitle}>Follow Us</Text>
+		  <TouchableOpacity onPress={() => NavigationService.navigate('Categories',{itemId: 1, categoryName: "Regional"})}><Text style={styles.footerlinks}>Regional</Text></TouchableOpacity>
+		  <TouchableOpacity onPress={() => NavigationService.navigate('Categories',{itemId: 2, categoryName: "Nature"})}><Text style={styles.footerlinks}>Nature</Text></TouchableOpacity>
+		  <TouchableOpacity onPress={() => NavigationService.navigate('Categories',{itemId: 3, categoryName: "Seasonal"})}><Text style={styles.footerlinks}>Seasonal</Text></TouchableOpacity>
+		  <Text style={styles.ftitle}>Follow Us</Text> 
 		 <FlatList style={styles.block1}
           data={[
             {key: 'Facebook', url: '#'},
